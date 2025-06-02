c0a6d5c2420c0d55ba2985cf148387f374aea7cb
ahmedabdellatifss <ahmedabdellatifss@yahoo.com>
2025-06-02 17:49:26 +0300

    Create Kafka broker and produce event

---
+++ patient-service/src/main/java/com/pm/patientservice/kafka/kafkaProducer.java
@@ -0,0 +1,36 @@
+package com.pm.patientservice.kafka;
+
+import com.pm.patientservice.model.Patient;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
+
+@Service
+public class kafkaProducer {
+
+    private static final Logger log = LogManager.getLogger(kafkaProducer.class);
+    private final KafkaTemplate<String, byte[]> kafkaTemplate;
+
+    public kafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
+
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendEvent(Patient patient)
+    {
+        PatientEvent event = PatientEvent.newBuilder()
+                .setPatientId(patient.getId().toString())
+                .setName(patient.getName())
+                .setEmail(patient.getEmail())
+                .setEventType("PATIENT_CREATED")
+                .build();
+        try {
+            kafkaTemplate.send("patient", event.toByteArray());
+        }catch (Exception e){
+            log.error("Error sending patient created event: {}", event);
+        }
+    }
+
+}
