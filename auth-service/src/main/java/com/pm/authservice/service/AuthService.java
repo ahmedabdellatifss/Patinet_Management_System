4a78e7d7d1f678f5178366a1de59cfb8cad8f2b1
ahmedabdellatifss <ahmedabdellatifss@yahoo.com>
2025-06-20 20:37:38 +0300

    Validate token testing

--- auth-service/src/main/java/com/pm/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/pm/authservice/service/AuthService.java
@@ -4,6 +4,7 @@ import com.pm.authservice.dto.LoginRequestDTO;
 import com.pm.authservice.model.User;
 import com.pm.authservice.repository.UserRepository;
 import com.pm.authservice.util.JwtUtil;
+import io.jsonwebtoken.JwtException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -31,6 +32,16 @@ public class AuthService{
 
         return token;
 
+    }
+
+    public boolean validateToken(String token)
+    {
+        try {
+            jwtUtil.validateToken(token);
+            return true;
+        }catch (JwtException e){
+            return false;
+        }
     }
 
 }
